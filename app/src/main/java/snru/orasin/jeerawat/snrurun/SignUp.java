e1daa6510d5eb87685f862c29d3c2cfbae9fcaee
Jeerawat <Jeerawatorasin@gmail.com>
2016-05-16 16:07:14 -0700

    AlertDialog finish

--- app/src/main/java/snru/orasin/jeerawat/snrurun/SignUp.java
+++ app/src/main/java/snru/orasin/jeerawat/snrurun/SignUp.java
@@ -14,6 +14,7 @@ public class SignUp extends AppCompatActivity {
     private RadioGroup radioGroup;
     private RadioButton choice1RadioButton, choice2RadioButton,
             choice3RadioButton,choice4RadioButton,choice5RadioButton;
+    private String nameString, userString, passwordString, avatarString;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,11 +22,42 @@ public class SignUp extends AppCompatActivity {
         setContentView(R.layout.activity_sign_up);
 
         //Bind Widget
+        bindwidget();
 
 
     }//Main Method
 
+    private void bindwidget() {
+
+        nameEditText = (EditText) findViewById(R.id.editText);
+        userEditText = (EditText) findViewById(R.id.editText2);
+        passwordEditText = (EditText) findViewById(R.id.editText3);
+        radioGroup = (RadioGroup) findViewById(R.id.ragAvata);
+        choice1RadioButton = (RadioButton) findViewById(R.id.radioButton);
+        choice2RadioButton = (RadioButton) findViewById(R.id.radioButton2);
+        choice3RadioButton = (RadioButton) findViewById(R.id.radioButton3);
+        choice4RadioButton = (RadioButton) findViewById(R.id.radioButton4);
+        choice5RadioButton = (RadioButton) findViewById(R.id.radioButton5);
+
+    }
+
     public void clickSignUpSign(View view) {
+
+        //Get Value From EditText
+        nameString = nameEditText.getText().toString().trim();
+        userString = userEditText.getText().toString().intern();
+        passwordString = passwordEditText.getText().toString().trim();
+
+        //Check Space
+        if (nameString.equals("") || userString.equals("") || passwordString.equals("") ) {
+
+            MyAlert myAlert = new MyAlert();
+            myAlert.myDialog(this,"มีช่องว่าง","กรุณากรองทุกช่องครับ");
+        }
+        else {
+
+        }
+
 
     }
 }//Main Class
